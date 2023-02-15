5f1de3ea11fc1ccf39420e7291a6425c39c15614
David Fokkema <davidfokkema@icloud.com>
2023-02-15 17:44:23 +0100

    Add progress bar and fake deletion

--- src/prune_envs/app.py
+++ src/prune_envs/app.py
@@ -1,27 +1,42 @@
 from textual.app import App, ComposeResult
 from textual.widgets import ListView, ListItem, Label, Footer, Static
-import threading
+from rich.progress import Progress, TextColumn, BarColumn
 
 
 class EnvironmentsList(ListView):
 
     BINDINGS = [("q", "quit", "Quit"), ("d", "mark_deletion", "Toggle Delete/Undelete")]
 
-    def action_mark_deletion(self):
+    def action_mark_deletion(self) -> None:
         self.highlighted_child.delete()
 
 
 class Environment(ListItem):
+    counts = 0
+
     def __init__(self, env_name: str) -> None:
         super().__init__()
         self.env_name = env_name
 
     def compose(self) -> ComposeResult:
-        yield Label(self.env_name)
+        yield Label(self.env_name, id="env_name")
         yield Static(id="status")
 
-    def delete(self):
+    def delete(self) -> None:
         self.add_class("delete")
+        self._progress = Progress(
+            TextColumn("[progress.description]{task.description}"),
+            BarColumn(bar_width=None),
+        )
+        self._progress.add_task("Deleting...", total=None)
+        self.timer = self.set_interval(1 / 60, self.update_progress)
+
+    def update_progress(self) -> None:
+        self.query_one("#status").update(self._progress)
+        self.counts += 1
+        if self.counts > 180:
+            self.query_one("#status").update("Deleted.")
+            self.timer.stop()
 
 
 class ListViewExample(App):
