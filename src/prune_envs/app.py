cae9bfc2c8b9ab09a42b515e43ede416421df9cd
David Fokkema <davidfokkema@icloud.com>
2023-02-15 16:33:06 +0100

    Use D to toggle deletion, plus CSS

--- src/prune_envs/app.py
+++ src/prune_envs/app.py
@@ -2,12 +2,21 @@ from textual.app import App, ComposeResult
 from textual.widgets import ListView, ListItem, Label, Footer
 
 
+class EnvironmentsList(ListView):
+
+    BINDINGS = [("d", "toggle_deletion", "Delete/Undelete")]
+
+    def action_toggle_deletion(self):
+        self.highlighted_child.toggle_class("delete")
+        print(self.highlighted_child.classes)
+
+
 class ListViewExample(App):
 
-    CSS_PATH = "list_view.css"
+    CSS_PATH = "app.css"
 
     def compose(self) -> ComposeResult:
-        yield ListView(
+        yield EnvironmentsList(
             ListItem(Label("One")),
             ListItem(Label("Two")),
             ListItem(Label("Three")),
