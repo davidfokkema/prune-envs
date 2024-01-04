e97422a55035f2715ba85c649db5100a8e655117
David Fokkema <davidfokkema@icloud.com>
2024-01-04 11:51:18 +0100

    Added docstrings, type hints and some cleanup

--- src/prune_envs/app.py
+++ src/prune_envs/app.py
@@ -13,40 +13,60 @@ from prune_envs import conda
 
 
 class EnvironmentsList(ListView):
+    """A widget showing a list of conda environments."""
+
     BINDINGS = [("d", "mark_deletion", "Delete")]
 
     async def on_list_view_highlighted(self, highlighted: ListView.Highlighted) -> None:
+        """Make sure the hightlighed item does not scroll out of view."""
         highlighted.item.scroll_visible()
 
     def action_mark_deletion(self) -> None:
+        """Delete the highlighted conda environment."""
         self.highlighted_child.delete()
 
 
 class EnvironmentItem(ListItem):
+    """A widget showing an individual conda environment."""
+
     app: "PruneEnvironments"
 
     delete_worker = None
     _spinner = Spinner(name="simpleDots")
 
-    def __init__(self, env: tuple) -> None:
+    def __init__(self, env: tuple[str, int]) -> None:
+        """Initialize the class.
+
+        Args:
+            env (tuple[str, int]): the name and creation timestamp of the
+                environment.
+        """
         super().__init__()
         self.env_name, self.env_ctime = env
         self.update_timer = self.set_interval(1 / 60, self.update_progress, pause=True)
 
     def compose(self) -> ComposeResult:
+        """Compose the user interface showing the name and creation time."""
         yield Label(self.env_name, id="env_name")
         yield Label(
             time.strftime("%b %d, %Y", time.localtime(self.env_ctime)), id="env_ctime"
         )
-        yield Horizontal(Static(id="status_msg"), Static(id="spinner"), id="status")
+        with Horizontal(id="status"):
+            yield Static(id="status_msg")
+            yield Static(id="spinner")
 
     def delete(self) -> None:
+        """Start deleting the conda environment."""
         if not "delete" in self.classes:
             self.add_class("delete")
             self.delete_worker = self.remove_environment()
 
     @work()
     async def remove_environment(self) -> None:
+        """Remove the conda environment.
+
+        This worker asynchronously updates the user interface to indicate that the environment is currently being removed, starts the removal process and awaits its completion. Finally, the user interface is updated to indicate that the removal process is finished.
+        """
         self.query_one("#status_msg").update("Deleting")
         self.update_timer.resume()
 
@@ -54,6 +74,7 @@ class EnvironmentItem(ListItem):
 
         self.update_timer.stop()
         try:
+            # update the spinner widget to an empty value
             self.query_one("#spinner").update()
             self.query_one("#status_msg").update("Deleted")
         except NoMatches:
@@ -62,6 +83,7 @@ class EnvironmentItem(ListItem):
             pass
 
     def update_progress(self) -> None:
+        """Update the progress spinner."""
         try:
             self.query_one("#spinner").update(self._spinner)
         except NoMatches:
@@ -70,34 +92,42 @@ class EnvironmentItem(ListItem):
             pass
 
     async def on_unmount(self):
+        """Wait for the worker to finish before shutdown."""
         if self.delete_worker:
             await self.delete_worker.wait()
 
 
 class WaitScreen(ModalScreen):
+    """A modal wait screen displaying a message."""
+
     def __init__(self, msg: str, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.msg = msg
 
     def compose(self) -> ComposeResult:
+        """Compose the user interface displaying a main message."""
         with Vertical(id="modal_wait"):
             with Center():
                 yield Label(self.msg, id="msg")
             yield LoadingIndicator()
 
 
-class PruneEnvironments(App):
+class PruneEnvironments(App[None]):
+    """The main app class."""
+
     CSS_PATH = "app.tcss"
     BINDINGS = [("q", "quit", "Quit"), ("ctrl+s", "save_screenshot()", None)]
 
     conda_lock = asyncio.Lock()
 
-    async def on_compose(self):
+    async def on_compose(self) -> None:
+        """Compose the wait screen and load environments."""
         await self.push_screen(WaitScreen("Looking for conda environments..."))
         self.envs = conda.get_environments()
         self.pop_screen()
 
     def compose(self) -> ComposeResult:
+        """Compose the main user interface showing a list of environments."""
         with Center():
             yield Label(
                 "Please select an environment and press 'D' to delete", id="instruction"
@@ -107,15 +137,14 @@ class PruneEnvironments(App):
         )
         yield Footer()
 
-    def on_mount(self) -> None:
-        self.screen.focus_next()
-
     async def action_quit(self) -> None:
+        """Run cleanup procedure on quitting the app."""
         self.query_one("EnvironmentsList").remove()
         await self.push_screen(WaitScreen("Waiting on running cleanups..."))
         await super().action_quit()
 
     def action_save_screenshot(self) -> None:
+        """Save a screenshot and notify the user."""
         path = self.save_screenshot()
         self.notify(f"Screen saved to {path}")
 
@@ -124,6 +153,7 @@ app = PruneEnvironments()
 
 
 def main():
+    """Main app entry point."""
     app.run()
 
 
--- src/prune_envs/conda.py
+++ src/prune_envs/conda.py
@@ -4,6 +4,14 @@ import subprocess
 
 
 def get_environments() -> list[tuple]:
+    """Get a list of conda environments.
+
+    Returns a list of conda environments where each item is a tuple consisting
+    of the name of the environment and its creation timestamp.
+
+    Returns:
+        list[tuple]: the list of environments
+    """
     process = subprocess.run("conda env list", shell=True, capture_output=True)
     envs = []
     for line in process.stdout.decode().splitlines():
@@ -18,7 +26,17 @@ def get_environments() -> list[tuple]:
     return envs
 
 
-async def remove_environment(name, lock):
+async def remove_environment(name: str, lock: asyncio.Lock) -> None:
+    """Remove a conda environment.
+
+    Remove the named conda environment. The process is locked using the provided
+    lock to ensure that the `conda env remove` command is not run concurrently
+    which breaks the removal process.
+
+    Args:
+        name (str): the name of the environment to remove. lock (asyncio.Lock):
+        the lock to guard the removal process.
+    """
     async with lock:
         process = await asyncio.create_subprocess_shell(
             f"conda env remove -n {name}",
